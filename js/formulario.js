070eda46df49b1c8923f12df81aceb9cabe25257
Alphyon <alphyon21@yahoo.es>
2015-02-20 07:36:11 -0600

    modificaciones js y css

--- js/formulario.js
+++ js/formulario.js
@@ -5,24 +5,43 @@ var $form = $('#formulario'),
     $list = $('#contenido'),
     $post = $('.item').first();
 
+    if (localStorage.getItem('autosave')) {
+      $titulo.val(sessionStorage.getItem('titulo'));
+      $url.val(sessionStorage.getItem('url'));
+    }
+
+    var id = setInterval(function(){
+      sessionStorage.setItem('titulo',$titulo.val());
+      sessionStorage.setItem('url',$url.val());
+    },1000);
     function mostrarFormulario() {
       $form.slideToggle();
+      $list.slideToggle();
+      return false;
+    }
+    function mostrarOcultarFormulario() {
+      $form.slideToggle();
+      $list.slideToggle();
       return false;
     }
     function agregarPost() {
       var url = $url.val(),
           titulo = $titulo.val(),
           $clone = $post.clone();
-
           $clone.find('.titulo_item a')
           .text(titulo)
           .attr('href',url);
           $clone.hide();
+          mostrarOcultarFormulario();
+          $titulo.val('');
+          $url.val('');
           $list.prepend($clone);
           $clone.fadeIn();
        return false;
     }
 
+    $('#publicar nav a').click(mostrarOcultarFormulario)
+
     //eventos
-    $button.click(mostrarFormulario);
+    $button.click(mostrarOcultarFormulario);
     $form.on('submit',agregarPost);
